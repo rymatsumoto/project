9e3b5635cfb4ef18b4c4cf03a3b9a541b12d8632
hflab-github <hflab@edu.k.u-tokyo.ac.jp>
2022-07-30 16:47:36 +0900

    AD変換タイミングを変更＋リファクタリング

--- const_voltage_pwmc_sequence/const_voltage_pwmc_sequence.c
+++ const_voltage_pwmc_sequence/const_voltage_pwmc_sequence.c
@@ -13,14 +13,14 @@
 
 #define INV_FREQ 85000
 #define DEAD_TIME 300
-// #define REF_RECT 1108
 #define REF_PWMC 260
-// #define REF_ZC_CNT 70
-
-#define SMA_LEN 20
+#define SMA_LEN 50
+#define TIMER0_INTERVAL 5
+#define TIMER1_INTERVAL 2000
+#define TIMER2_INTERVAL 1000
 
 volatile int REF_RECT = 1108;
-volatile int REF_ZC_CNT = 80;
+volatile int REF_ZC_CNT = 68;
 
 volatile int set_pwm_on_trans = -1;
 volatile int set_pwm_on_rect = -1;
@@ -44,7 +44,7 @@ volatile float integralGain = 0.03;
 float proportional = 0;
 float integral = 0;
 
-volatile float i2dc;
+float i2dc;
 float range[] = {5., 5., 5., 5., 5., 5., 5., 5.};
 float data[] = {0., 0., 0., 0.};
 
@@ -73,10 +73,8 @@ volatile float pwmc_rx_update = 0;
 // 送電側可変キャパシタ制御
 //----------------------------------------------------------------------------------------
 
-interrupt void pwmc_tx_control(void)
+void pwmc_tx_control(void)
 {
-	C6657_timer0_clear_eventflag();
-
 	if (pwmc_tx_control_on == 1) {
 		proportional = zc_cnt_std * proportionalGain;       // 比例ゲイン演算
 		integral = integral + zc_cnt_std * integralGain;    // 積分ゲイン演算
@@ -98,6 +96,44 @@ interrupt void pwmc_tx_control(void)
 		IPFPGA_write(BDN0, 0x04, wref_BDN0);
 	}
 }
+
+//----------------------------------------------------------------------------------------
+// I2dcの移動平均を取得
+//----------------------------------------------------------------------------------------
+
+void read_i2dc(void)
+{
+	PEV_ad_in_4ch(BDN2, 0, data);
+	i2dc = data[0] * 25;							// 2V 50A
+
+	int i;
+
+	for (i = 0; i < SMA_LEN - 1; ++i) {             // 移動平均リストの各要素を一つずつ左へシフト 
+		i2dc_lst_tmp[i] = i2dc_lst[i+1];
+	}
+	i2dc_lst_tmp[SMA_LEN-1] = i2dc;	                // 移動平均リストの末尾に最新の測定値を追加
+	for (i = 0; i < SMA_LEN; ++i) {
+		i2dc_lst[i] = i2dc_lst_tmp[i];
+	}
+	i2dc_sum = 0;
+	for (i = 0; i < SMA_LEN; ++i) {                 // 移動平均リストの和
+		i2dc_sum += i2dc_lst[i];
+	}
+	i2dc_avg = i2dc_sum / SMA_LEN; 		            // 要素数で除算
+}
+
+//----------------------------------------------------------------------------------------
+// 送電側制御＋I2dc取得
+//----------------------------------------------------------------------------------------
+
+interrupt void pwmc_tx_control_read_i2dc(void)
+{
+	C6657_timer0_clear_eventflag();
+
+	pwmc_tx_control();
+	read_i2dc();
+}
+
 //----------------------------------------------------------------------------------------
 // 受電側可変キャパシタ制御
 //----------------------------------------------------------------------------------------
@@ -145,6 +181,15 @@ interrupt void pwmc_rx_control(void)
 		wref_BDN1 = pwm_out_BDN1 * carrier_cnt_max;
 		IPFPGA_write(BDN1, 0x04, wref_BDN1);
 	}
+}
+
+//----------------------------------------------------------------------------------------
+// カウンター
+//----------------------------------------------------------------------------------------
+
+interrupt void increment_counter(void)
+{
+	C6657_timer2_clear_eventflag();
 
 	if (count_start == 1){
 		counter = counter + 1;
@@ -155,8 +200,10 @@ interrupt void pwmc_rx_control(void)
 // V1とI1の位相差を取得
 //----------------------------------------------------------------------------------------
 
-void read_zc_cnt(void)
+interrupt void read_zc_cnt(void)
 {
+	int0_ack();
+
 	zc_cnt = IPFPGA_read(BDN0, 0x19);
 
 	if (zc_cnt <= carrier_cnt_max) {
@@ -171,49 +218,13 @@ void read_zc_cnt(void)
 }
 
 //----------------------------------------------------------------------------------------
-// I2dcの移動平均を取得
-//----------------------------------------------------------------------------------------
-
-void read_i2dc_avg(void)
-{
-	PEV_ad_in_4ch(BDN2, 0, data);
-	i2dc = data[0] * 25;							// 2V 50A
-
-	int i;
-
-	for (i = 0; i < SMA_LEN - 1; ++i) {             // 移動平均リストの各要素を一つずつ左へシフト 
-		i2dc_lst_tmp[i] = i2dc_lst[i+1];
-	}
-	i2dc_lst_tmp[SMA_LEN-1] = i2dc;	                // 移動平均リストの末尾に最新の測定値を追加
-	for (i = 0; i < SMA_LEN; ++i) {
-		i2dc_lst[i] = i2dc_lst_tmp[i];
-	}
-	i2dc_sum = 0;
-	for (i = 0; i < SMA_LEN; ++i) {                 // 移動平均リストの和
-		i2dc_sum += i2dc_lst[i];
-	}
-	i2dc_avg = i2dc_sum / SMA_LEN; 		            // 要素数で除算
-}
-
-//----------------------------------------------------------------------------------------
-// フィードバック情報取得
-//----------------------------------------------------------------------------------------
-
-interrupt void read_feedback_signal(void)
-{
-	int0_ack();
-	read_zc_cnt();
-	read_i2dc_avg();
-}
-
-//----------------------------------------------------------------------------------------
 // 初期化
 //----------------------------------------------------------------------------------------
 
 void initialize(void)
 {
 	PEV_ad_set_range(BDN2, range);
-	PEV_ad_set_mode(BDN2, 1);
+	PEV_ad_set_mode(BDN2, 0);
 
 	int i;
 
@@ -248,15 +259,19 @@ void initialize(void)
 	PEV_inverter_init(BDN2, INV_FREQ, DEAD_TIME);
 	PEV_inverter_init_int_timing(BDN2, 1, 0, 0);
 	PEV_int_init(BDN2, 2, 0, 0, 0, 0, 0, 0, 0);
-	int0_init_vector(read_feedback_signal, (CSL_IntcVectId)6, FALSE);
+	int0_init_vector(read_zc_cnt, (CSL_IntcVectId)6, FALSE);
 
-	C6657_timer0_init(5);
-	C6657_timer0_init_vector(pwmc_tx_control, (CSL_IntcVectId)5);
+	C6657_timer0_init(TIMER0_INTERVAL);
+	C6657_timer0_init_vector(pwmc_tx_control_read_i2dc, (CSL_IntcVectId)5);
 	C6657_timer0_start();
 
-	C6657_timer1_init(1000);
+	C6657_timer1_init(TIMER1_INTERVAL);
 	C6657_timer1_init_vector(pwmc_rx_control, (CSL_IntcVectId)7);
 	C6657_timer1_start();
+
+	C6657_timer2_init(TIMER2_INTERVAL);
+	C6657_timer2_init_vector(increment_counter, (CSL_IntcVectId)8);
+	C6657_timer2_start();
 
 	PEV_inverter_enable_int(BDN2);
 
@@ -264,6 +279,7 @@ void initialize(void)
 
 	C6657_timer0_enable_int();
 	C6657_timer1_enable_int();
+	C6657_timer2_enable_int();
 
 	int_enable();
 }
