aac5ba36174d743fbe3ac9d2624a69a478f09c29
hflab-github <hflab@edu.k.u-tokyo.ac.jp>
2022-09-17 17:40:45 +0900

    create search_pulse_road_side

---
+++ search_pulse_road_side/search_pulse_road_side.c
@@ -0,0 +1,16 @@
+/*********************************************************
+ Title:		Template source file for c6657
+ Copyright:	1999-2013  Myway Plus Co.,Ltd.
+
+ Abstract:
+	This C source code is the template program
+	that is used when you generate a project newly.
+
+ Model:	Expert4(c6657)
+*********************************************************/
+volatile float	f;
+
+void	MW_main(void)
+{
+	f = 0.0f;
+}
