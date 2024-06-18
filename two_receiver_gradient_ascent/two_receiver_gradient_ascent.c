9697a73d739dcac666fcbddd3dae97746a99fe31
rymatsumoto <ryo.seaskate@gmail.com>
2024-06-18 12:08:34 +0900

    update two_receiver_gradient_ascent and delete two_receiver_hill_climbing

--- two_receiver_gradient_ascent/two_receiver_gradient_ascent.c
+++ two_receiver_gradient_ascent/two_receiver_gradient_ascent.c
@@ -10,34 +10,25 @@
 *********************************************************/
 #include <mwio4.h>
 
-#define BDN_PEV 0   //PEVボード
-#define BDN_FPGA1 0 //FPGAボードRX1
-#define BDN_FPGA2 1 //FPGAボードRX2
+#define BDN_PEV 0    //PEVボード
+#define BDN_FPGA1 0  //FPGAボードRX1
+#define BDN_FPGA2 1  //FPGAボードRX2
 
 			                //WN WP VN VP UN UP
 #define TRANS_MODE 80 		//00 00 01 01 00 00
 #define INV_FREQ 100000
 #define DEAD_TIME 500
 #define TIMER0_INTERVAL 10
-#define TIMER1_INTERVAL 300e3
-#define TIMER2_INTERVAL 1e3
-#define RL1 10
-#define RL2 10
+#define TIMER1_INTERVAL 200e3
 
 #define FPGA_CLK_FREQ 100e6
 #define FRAC_WIDTH 32
 
-#define POWER_LOG_SIZE 200
+#define LPF_CUTOFF_0 200e3
+#define LPF_CUTOFF_1 1e2
 
 #define REF_PWMC_RX1 225
 #define REF_PWMC_RX2 225
-
-#define LPF_CUTTOFF_dsp 2
-#define LPF_CUTTOFF_0 200e3
-#define LPF_CUTTOFF_1 1e2
-#define LPF_CUTTOFF_2 1e2
-
-#define PI 3.14
 
 #define INCREMENT_RX1 1
 #define DECREMENT_RX1 2
@@ -48,9 +39,9 @@
 
 #define DELTA 0.025
 
-volatile float alpha = 1e-5;
-volatile float epsilon = 1e-3;
+#define PI 3.14
 
+// イネーブル指令
 volatile int start_inv_w_o_control = 0;
 volatile int start_current_control = 0;
 volatile int start_pwmc_rx1_init = 0;
@@ -59,14 +50,30 @@ volatile int start_pwmc_control = 0;
 int current_control_on = 0;
 int pwmc_control_on = 0;
 
+// 制御なしデューティ
+volatile float duty_w_o_control = 0;
+
+// FPGA LPF設定
+float lpf_T_0;
+float lpf_A_0;
+float lpf_B_0;
+float lpf_T_1;
+float lpf_A_1;
+float lpf_B_1;
+
+// FPGA AD変換結果読み出し
 INT32 ad_0_data_lpf_peak;
 INT32 ad_1_data_lpf_crnt;
-INT32 ad_2_data_lpf_crnt;
-INT32 ad_3_data_lpf_crnt;
+
+// DC電流読み出し
+const float Gth = 0.1042;
+float dc_current_lpf = 0;
+float dc_current_avg = 0;
+
+// 電流制御
 float itx_ampl = 0;
 volatile float itx_weight = 1.23;
 volatile float itx_ampl_ref;
-
 float error_crnt = 0;
 float error_prvs = 0;
 float error_integral = 0;
@@ -77,67 +84,28 @@ volatile float vdc = 30;
 volatile float proportionalGain = 1;
 volatile float integralGain = 1e3;
 
+// PWMC設定
 volatile float pwm_out_rx1 = 0;
 volatile float pwm_out_rx2 = 0;
-volatile int wref_rx1 = 0;
-volatile int wref_rx2 = 0;
-volatile int carrier_cnt_max;
-volatile int carrier_cnt_hlf;
+int carrier_cnt_max;
+int carrier_cnt_hlf;
+int wref_rx1 = 0;
+int wref_rx2 = 0;
 
-float range[] = {5., 5., 5., 5., 5., 5., 5., 5.};
-float data[] = {0., 0., 0., 0.};
-float dc_current_rx1 = 0;
-float dc_current_rx2 = 0;
-float dc_current_rx1_lpf = 0;
-float dc_current_rx2_lpf = 0;
-float dc_current_rx1_prvs = 0;
-float dc_current_rx2_prvs = 0;
-float dc_current_rx1_lpf_prvs = 0;
-float dc_current_rx2_lpf_prvs = 0;
-
-volatile float dc_current_rx1_weight = 1;
-volatile float dc_current_rx2_weight = 1;
-
-float lpf_T_dsp;
-float lpf_A_dsp;
-float lpf_B_dsp;
-
-float lpf_T_0;
-float lpf_A_0;
-float lpf_B_0;
-
-float lpf_T_1;
-float lpf_A_1;
-float lpf_B_1;
-
-float lpf_T_2;
-float lpf_A_2;
-float lpf_B_2;
-
-const float Gth = 0.1042;
-
-float power_rx1 = 0;
-float power_rx2 = 0;
-float power_total = 0;
-float power_total_avg = 0;
-float power_total_updated = 0;
-
-float power_log[POWER_LOG_SIZE];
-
-volatile float duty_w_o_control = 1;
-
+// 可変C制御
 int state = INCREMENT_RX1;
-
-float power_total_increment_rx1 = 0;
-float power_total_decrement_rx1 = 0;
-float power_total_increment_rx2 = 0;
-float power_total_decrement_rx2 = 0;
-
+float dc_current_updated = 0;
+volatile float alpha = 1e-5;
+volatile float epsilon = 1e-3;
+float dc_current_increment_rx1 = 0;
+float dc_current_decrement_rx1 = 0;
+float dc_current_increment_rx2 = 0;
+float dc_current_decrement_rx2 = 0;
 float gradient_rx1 = 0;
 float gradient_rx2 = 0;
 
 //----------------------------------------------------------------------------------------
-//　移動平均
+//　移動平均（使ってない）
 //----------------------------------------------------------------------------------------
 
 float moving_average(float* moving_average_array_p, int array_size, float update_value)
@@ -182,18 +150,14 @@ int convert_binary(float f)
 
     float divisor = 0.5;
 
-    for (i = 0; i < FRAC_WIDTH; i++)
-    {
+    for (i = 0; i < FRAC_WIDTH; i++) {
         q = r / divisor;
         b[i] = q;
         r = r - divisor * q;
         divisor = divisor / 2;
     }
-
-    for (i = 0; i < FRAC_WIDTH; i++)
-    {
+    for (i = 0; i < FRAC_WIDTH; i++) {
         int pow = 1;
-
         for (j = 0; j < i; j++)
         {
             pow = 2 * pow;
@@ -226,88 +190,16 @@ float limitter(float input_value, float lower_limit, float upper_limit)
 }
 
 //----------------------------------------------------------------------------------------
-//　出力電流読み出し
+//　送電側DC電流読み出し
 //----------------------------------------------------------------------------------------
 
 interrupt void read_dc_current(void)
 {
 	C6657_timer0_clear_eventflag();
 
-	// PEV_ad_in_4ch(BDN_PEV, 0, data);
-	// dc_current_rx1 = (2.5 - data[0]) / Gth / 3;
-	// dc_current_rx2 = (2.5 - data[1]) / Gth / 3;
-
 	ad_1_data_lpf_crnt = IPFPGA_read(BDN_FPGA1, 0x18);
-	ad_2_data_lpf_crnt = IPFPGA_read(BDN_FPGA1, 0x19);
-
-	dc_current_rx1 = (2.5 - ad_1_data_lpf_crnt * 5. / 8000.) / Gth / 3 * dc_current_rx1_weight;
-	dc_current_rx2 = (2.5 - ad_2_data_lpf_crnt * 5. / 8000.) / Gth / 3 * dc_current_rx2_weight;;
-
-	dc_current_rx1_lpf = lpf_A_dsp * dc_current_rx1 + lpf_A_dsp * dc_current_rx1_prvs + lpf_B_dsp * dc_current_rx1_lpf_prvs;
-	dc_current_rx2_lpf = lpf_A_dsp * dc_current_rx2 + lpf_A_dsp * dc_current_rx2_prvs + lpf_B_dsp * dc_current_rx2_lpf_prvs;
-
-	dc_current_rx1_prvs = dc_current_rx1;
-	dc_current_rx2_prvs = dc_current_rx2;
-	dc_current_rx1_lpf_prvs = dc_current_rx1_lpf;
-	dc_current_rx2_lpf_prvs = dc_current_rx2_lpf;
-}
-
-//----------------------------------------------------------------------------------------
-//　可変キャパシタ制御
-//----------------------------------------------------------------------------------------
-
-interrupt void pwmc_control(void)
-{
-	C6657_timer1_clear_eventflag();
-
-	if (pwmc_control_on == 1) {
-
-		power_total_updated = power_total_avg;
-
-		if (state == INCREMENT_RX1) {
-			pwm_out_rx1 += DELTA;
-			state = DECREMENT_RX1;
-		}
-		else if (state == DECREMENT_RX1) {
-			power_total_increment_rx1 = power_total_updated;
-			pwm_out_rx1 -= 2*DELTA;
-			state = INCREMENT_RX2;
-		}
-		else if (state == INCREMENT_RX2) {
-			power_total_decrement_rx1 = power_total_updated;
-			pwm_out_rx1 += DELTA;
-			pwm_out_rx2 += DELTA;
-			state = DECREMENT_RX2;
-		}
-		else if (state == DECREMENT_RX2) {
-			power_total_increment_rx2 = power_total_updated;
-			pwm_out_rx2 -= 2*DELTA;
-			state = UPDATE_RX1_RX2;
-		}
-		else if (state == UPDATE_RX1_RX2) {
-			power_total_decrement_rx2 = power_total_updated;
-			pwm_out_rx2 += DELTA;
-			gradient_rx1 = (power_total_increment_rx1 - power_total_decrement_rx1) / (2 * DELTA);
-			gradient_rx2 = (power_total_increment_rx2 - power_total_decrement_rx2) / (2 * DELTA);
-			if (mwabs(gradient_rx1) < epsilon && mwabs(gradient_rx2) < epsilon) {
-				state = STOP;
-			}
-			else {
-				pwm_out_rx1 += alpha * gradient_rx1;
-				pwm_out_rx2 += alpha * gradient_rx2;
-				state = INCREMENT_RX1;
-			}
-		}
-
-		pwm_out_rx1 = limitter(pwm_out_rx1, 0, 1);
-		pwm_out_rx2 = limitter(pwm_out_rx2, 0, 1);
-	}
-
-	wref_rx1 = pwm_out_rx1 * carrier_cnt_max;
-	wref_rx2 = pwm_out_rx2 * carrier_cnt_max;
-
-	IPFPGA_write(BDN_FPGA1, 0x04, wref_rx1);
-	IPFPGA_write(BDN_FPGA2, 0x04, wref_rx2);
+	dc_current_lpf = (ad_1_data_lpf_crnt * 5. / 8000. - 2.5) / Gth / 3;
+	dc_current_avg = dc_current_lpf;
 }
 
 //----------------------------------------------------------------------------------------
@@ -329,9 +221,7 @@ interrupt void current_control(void)
         error_prvs = error_crnt;
 
 		vtx_ampl_ref = limitter(vtx_ampl_ref, 0, vdc * 4 / PI);
-
         inv_mod = 2 / PI * arccos(PI * vtx_ampl_ref / (4 * vdc));
-
         inv_mod = limitter(inv_mod, 0, 1);
 
         PEV_inverter_set_uvw(BDN_PEV, -inv_mod, inv_mod, 0, 0);
@@ -343,19 +233,59 @@ interrupt void current_control(void)
 }
 
 //----------------------------------------------------------------------------------------
-//　受電電力の合計を計算
+//　可変キャパシタ制御
 //----------------------------------------------------------------------------------------
 
-interrupt void calculate_total_power(void)
+interrupt void pwmc_control(void)
 {
-	C6657_timer2_clear_eventflag();
+	C6657_timer1_clear_eventflag();
 
-	power_rx1 = RL1 * dc_current_rx1_lpf * dc_current_rx1_lpf;
-	power_rx2 = RL2 * dc_current_rx2_lpf * dc_current_rx2_lpf;
+	if (pwmc_control_on == 1) {
+		dc_current_updated = dc_current_avg;
 
-	power_total = power_rx1 + power_rx2;
+		if (state == INCREMENT_RX1) {
+			pwm_out_rx1 += DELTA;
+			state = DECREMENT_RX1;
+		}
+		else if (state == DECREMENT_RX1) {
+			dc_current_increment_rx1 = dc_current_updated;
+			pwm_out_rx1 -= 2*DELTA;
+			state = INCREMENT_RX2;
+		}
+		else if (state == INCREMENT_RX2) {
+			dc_current_decrement_rx1 = dc_current_updated;
+			pwm_out_rx1 += DELTA;
+			pwm_out_rx2 += DELTA;
+			state = DECREMENT_RX2;
+		}
+		else if (state == DECREMENT_RX2) {
+			dc_current_increment_rx2 = dc_current_updated;
+			pwm_out_rx2 -= 2*DELTA;
+			state = UPDATE_RX1_RX2;
+		}
+		else if (state == UPDATE_RX1_RX2) {
+			dc_current_decrement_rx2 = dc_current_updated;
+			pwm_out_rx2 += DELTA;
+			gradient_rx1 = (dc_current_increment_rx1 - dc_current_decrement_rx1) / (2 * DELTA);
+			gradient_rx2 = (dc_current_increment_rx2 - dc_current_decrement_rx2) / (2 * DELTA);
+			if (mwabs(gradient_rx1) < epsilon && mwabs(gradient_rx2) < epsilon) {
+				state = STOP;
+			}
+			else {
+				pwm_out_rx1 += alpha * gradient_rx1;
+				pwm_out_rx2 += alpha * gradient_rx2;
+				state = INCREMENT_RX1;
+			}
+		}
 
-	power_total_avg = moving_average(power_log, POWER_LOG_SIZE, power_total);
+		pwm_out_rx1 = limitter(pwm_out_rx1, 0, 1);
+		pwm_out_rx2 = limitter(pwm_out_rx2, 0, 1);
+	}
+	wref_rx1 = pwm_out_rx1 * carrier_cnt_max;
+	wref_rx2 = pwm_out_rx2 * carrier_cnt_max;
+
+	IPFPGA_write(BDN_FPGA1, 0x04, wref_rx1);
+	IPFPGA_write(BDN_FPGA2, 0x04, wref_rx2);
 }
 
 //----------------------------------------------------------------------------------------
@@ -364,43 +294,22 @@ interrupt void calculate_total_power(void)
 
 void initialize(void)
 {
-	int i;
-
-	for (i = 0; i < POWER_LOG_SIZE; i++) {
-		power_log[i] = 0;
-	}
-
-	lpf_T_dsp = 1 / (2 * PI * LPF_CUTTOFF_dsp);
-	lpf_A_dsp = TIMER0_INTERVAL * 1e-6 / (TIMER0_INTERVAL * 1e-6 + 2 * lpf_T_dsp);
-	lpf_B_dsp = (2 * lpf_T_dsp - TIMER0_INTERVAL * 1e-6) / (2 * lpf_T_dsp + TIMER0_INTERVAL * 1e-6);
-
-	lpf_T_0 = 1 / (2 * PI * LPF_CUTTOFF_0);
+	// FPGA LPF設定
+	lpf_T_0 = 1 / (2 * PI * LPF_CUTOFF_0);
 	lpf_A_0 = 1. / FPGA_CLK_FREQ / (1. / FPGA_CLK_FREQ + 2 * lpf_T_0);
 	lpf_B_0 = (2 * lpf_T_0 - 1. / FPGA_CLK_FREQ) / (2 * lpf_T_0 + 1. / FPGA_CLK_FREQ);
 
-	lpf_T_1 = 1 / (2 * PI * LPF_CUTTOFF_1);
+	lpf_T_1 = 1 / (2 * PI * LPF_CUTOFF_1);
 	lpf_A_1 = 1. / FPGA_CLK_FREQ / (1. / FPGA_CLK_FREQ + 2 * lpf_T_1);
 	lpf_B_1 = (2 * lpf_T_1 - 1. / FPGA_CLK_FREQ) / (2 * lpf_T_1 + 1. / FPGA_CLK_FREQ);
 
-	lpf_T_2 = 1 / (2 * PI * LPF_CUTTOFF_2);
-	lpf_A_2 = 1. / FPGA_CLK_FREQ / (1. / FPGA_CLK_FREQ + 2 * lpf_T_2);
-	lpf_B_2 = (2 * lpf_T_2 - 1. / FPGA_CLK_FREQ) / (2 * lpf_T_2 + 1. / FPGA_CLK_FREQ);
-
 	IPFPGA_write(BDN_FPGA1, 0x15, convert_binary(lpf_A_0));
 	IPFPGA_write(BDN_FPGA1, 0x16, convert_binary(lpf_B_0));
-	
+
 	IPFPGA_write(BDN_FPGA1, 0x17, convert_binary(lpf_A_1));
 	IPFPGA_write(BDN_FPGA1, 0x18, convert_binary(lpf_B_1));
 
-	IPFPGA_write(BDN_FPGA1, 0x19, convert_binary(lpf_A_2));
-	IPFPGA_write(BDN_FPGA1, 0x1a, convert_binary(lpf_B_2));
-
-	IPFPGA_write(BDN_FPGA1, 0x09, REF_PWMC_RX1);
-	IPFPGA_write(BDN_FPGA2, 0x09, REF_PWMC_RX2);
-
-	PEV_ad_set_range(BDN_PEV, range);
-	PEV_ad_set_mode(BDN_PEV, 0);
-
+	// FPGA PWMC設定
 	carrier_cnt_max = 50000000. / INV_FREQ;
 	carrier_cnt_hlf = 25000000. / INV_FREQ;
 	
@@ -418,6 +327,7 @@ void initialize(void)
 	IPFPGA_write(BDN_FPGA2, 0x05, DEAD_TIME / 10);
 	IPFPGA_write(BDN_FPGA2, 0x09, REF_PWMC_RX2);
 
+	// 割り込み関数設定
 	int_disable();
 
 	PEV_init(BDN_PEV);
@@ -437,16 +347,12 @@ void initialize(void)
 	C6657_timer1_init_vector(pwmc_control, (CSL_IntcVectId)6);
 	C6657_timer1_start();
 
-	C6657_timer2_init(TIMER2_INTERVAL);
-	C6657_timer2_init_vector(calculate_total_power, (CSL_IntcVectId)7);
-	C6657_timer2_start();
-
     PEV_inverter_enable_int(BDN_PEV);
     int0_enable_int();
     C6657_timer0_enable_int();
 	C6657_timer1_enable_int();
-	C6657_timer2_enable_int();
-	int_enable();
+
+    int_enable();
 }
 
 //----------------------------------------------------------------------------------------
@@ -480,5 +386,5 @@ void MW_main(void)
 			pwmc_control_on = 1;
 			start_pwmc_control = -1;
 		}
-	}
+    }
 }
